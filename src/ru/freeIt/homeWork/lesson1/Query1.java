e0046a0711b5a4687bab7d1b334a2a54bf90e368
admin <root>
2021-03-24 11:11:07 +0300

    Add StudyBook homework task 7-8 and bugFix lesson2 task7

--- src/ru/freeIt/homeWork/lesson1/Query1.java
+++
@@ -1,7 +0,0 @@
-package ru.freeIt.homeWork.lesson1;
-//Набрать приведенный пример, откомпилировать его и запустить.
-public class Query1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
